94c8f39e61be0bf4a4c15671c68f8829bb75e2f8
Alejandro Sanz Martin <alexsm32@gmail.com>
2019-03-12 19:07:18 +0200

    copyyyyyy

--- app/src/main/java/com/example/toplist/ListContent.java
+++ app/src/main/java/com/example/toplist/ListContent.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,12 +16,14 @@ public class ListContent extends AppCompatActivity {
 
     List<String> items = new ArrayList<>();
     private ListView itemsList;
+    private TextView listName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_content);
 
+        listName = (TextView) findViewById(R.id.listName);
         itemsList =(ListView)findViewById(R.id.itemsList);
     }
 
@@ -39,6 +42,11 @@ public class ListContent extends AppCompatActivity {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
                 "toplist", null, 1);
         SQLiteDatabase bd = admin.getWritableDatabase();
+        Cursor fila2 = bd.rawQuery(
+                "select description from lists where id = "+listID, null);
+        if(fila2.moveToFirst()) {
+            listName.setText(fila2.getString(0));
+        }
         Cursor fila = bd.rawQuery(
                 "select description from items where id in (select iditem from relation where idlist = "+listID+")", null);
         while(fila.moveToNext()) {
